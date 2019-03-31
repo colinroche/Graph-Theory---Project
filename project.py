d4819b1767d0f282374759b0e70fe491412ff9ba
Colin Roche <G00349215@gmit.ie>
2019-03-31 00:45:15 +0000

    Thompson's Constructor - comments

--- project.py
+++ project.py
@@ -57,37 +57,54 @@ def thompsonCompiler(postfix):
 
   for c in postfix:
     if c == '*':
+      # Pop 1 NFA off of nfaStack
       nfa1 = nfaStack.pop()
+      # Create new accept and initail states
       initial = state()
       accept = state()
+      # Join new initial state to the NFA's initial state and the new accpet state
       initial.edge1 = nfa1.initial
       initial.edge2 = accept
+      # Join old accept state to new accept state and NFA's initial state
       nfa1.accept.edge1 = nfa.initial
       nfa1.accept.edge2 = accept
+      # Add new NFA containing these states to the nfaStack
       nfaStack.append(nfa(initial, accept))
 
     elif c == '.':
+      # Pop 2 NFA's off of nfaStack
       nfa2 = nfaStack.pop()
       nfa1 = nfaStack.pop()
+      # Join NFA's, first accept to second initial
       nfa1.accept.edge1 = nfa2.initial
+      # Add new NFA containing these states to the nfaStack
       nfaStack.append(nfa(nfa1.initial, nfa2.accept))
     
     elif c == '|':
+      # Pop 2 NFA's off of nfaStack
       nfa2 = nfaStack.pop()
       nfa1 = nfaStack.pop()
+      # Create new accept and initail states
       initial = state()
       accept = state()
+      # Join initial state to initial states of NFA's on stack
       initial.edge1 = nfa1.initial
       initial.edge2 = nfa2.initial
+      # Joining the accept states of the NFA's popped from stack to new accept state
       nfa1.accept.edge1 = accept
       nfa2.accept.edge1 = accept
+      # Add new NFA containing these states to the nfaStack
       nfaStack.append(nfa(initial, accept))
       
+    ## Normal char
     else:
+      # Create new accept and initail states
       accept = state()
       initial = state()
+      # Join the initial and accept states using an arrow labelled c
       initial.label = c
       initial.edge1 = accept
+      # Add new NFA containing these states to the nfaStack
       nfaStack.append(nfa(initial, accept))
       
   return nfaStack.pop()
